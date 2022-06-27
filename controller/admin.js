27a51f49469c8ccafd6bcdbf8952e59b2e64d8ed
Prabesh <prabesh.bista369@gmail.com>
2022-06-27 10:48:01 +0545

    fetched data from the database

--- controller/admin.js
+++ controller/admin.js
@@ -1,23 +1,39 @@
 const Product=require("../model/admin");
 
 
-
-
-
 const addProduct=(req,res)=>{
     res.status(200).render("admin/add-products");
 }
 
+
+//Posting the product
 const postProduct=async(req,res)=>{
     const title=req.body.title;
     const imageURL=req.body.imageurl;
     const price=req.body.price;
     const description=req.body.description;
 
-    const result=await new Product()
-    
+    //it will add the data fetched from the ejs template to postgresSql database
+   await Product.create({
+        title:title,
+        imageURL:imageURL,
+        price:price,
+        description:description
+    });
 
+    //it will redirect us to the add-products page ,after we have pushed data to database
+    res.status(200).redirect("/admin/add-products");
+    
+}
+
+//Retriving the data
+
+const getProducts=async(req,res)=>{
+    const products=await Product.findAll();
+    res.status(200).render("admin/products",{
+        title:products[1].title
+    });
 }
 
 
-module.exports={addProduct,postProduct};
+module.exports={addProduct,postProduct,getProducts};
--- routes/admin.js
+++ routes/admin.js
@@ -1,10 +1,11 @@
 const express=require("express");
 
 const router=express.Router();
-const  {addProduct,postProduct}=require("../controller/admin");
+const  {addProduct,postProduct,getProducts}=require("../controller/admin");
 
 
 router.get("/add-products/",addProduct);
-router.post("/add",postProduct)
+router.post("/add",postProduct);
+router.get("/products",getProducts);
 
 module.exports=router;
