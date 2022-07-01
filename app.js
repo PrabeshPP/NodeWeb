055606ab4c2caf53ad1a90e93faa048bdd90bea9
Prabesh <prabesh.bista369@gmail.com>
2022-07-01 10:12:08 +0545

    tailwind css added

--- app.js
+++ app.js
@@ -1,2 +1,19 @@
+const express=require("express");
+const path=require("path");
+const app=express();
+
+app.set("view engine","ejs");
+app.set("views","views");
+
+app.use(express.static(path.join(__dirname,"public")));
+
+app.get("/",(req,res)=>{
+
+    res.render("auth/login")
+});
+
+app.listen(3000,()=>{
+    console.log("Port Running at 3000")
+})
 
 
--- routes/shop.js
+++
@@ -1,8 +0,0 @@
-const express=require("express");
-const {getProducts,getDetailProduct}=require("../controller/shop");
-
-const router=express.Router();
-
-router.get("/",getProducts);
-
-module.exports=router;
